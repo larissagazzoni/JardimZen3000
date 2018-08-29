d4c407c2c009b125aab8887e59512fc65a1fd028
Larissa Yumi <gazzonilarissa@gmail.com>
2018-08-29 19:31:43 -0300

    add script draw with mouse e shader draw tracks Nao consegui colocar o script no plano n sei como resolver sos

---
+++ Zen/JardimZen3000/Assets/Draw with mouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawwithmouse : MonoBehaviour {
+    public Camera _Camera;
+    public Shader _drawShader;
+
+    private RenderTexture _splatmap;
+    private Material _sandMaterial, _drawMaterial; 
+
+	// Use this for initialization
+	void Start () {
+        _drawMaterial = new Material(_drawShader);
+        _drawMaterial.SetVector("_Color", Color.red);
+
+        _sandMaterial = GetComponent<MeshRenderer>().material;
+        _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        _sandMaterial.SetTexture("_Splat", _splatmap);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
