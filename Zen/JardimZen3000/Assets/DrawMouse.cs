d4870a5311eb589b32c74a496ebac922cf0c10f0
Larissa Yumi <gazzonilarissa@gmail.com>
2018-08-30 13:54:12 -0300

    some other stuff idk

--- Zen/JardimZen3000/Assets/DrawMouse.cs
+++ Zen/JardimZen3000/Assets/DrawMouse.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drawwithmouse : MonoBehaviour {
+public class Drawwithmouse : MonoBehaviour
+{
     public Camera _Camera;
     public Shader _drawShader;
 
     private RenderTexture _splatmap;
-    private Material _sandMaterial, _drawMaterial; 
+    private Material _sandMaterial, _drawMaterial;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         _drawMaterial = new Material(_drawShader);
         _drawMaterial.SetVector("_Color", Color.red);
 
         _sandMaterial = GetComponent<MeshRenderer>().material;
         _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
         _sandMaterial.SetTexture("_Splat", _splatmap);
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
