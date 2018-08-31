c0b465c6cdfb89f99fa278d38f076b223bb23e91
Leticia de Vicq <leticiadevicq@gmail.com>
2018-08-31 03:21:21 -0300

    zengarden3000 scene

---
+++ Zen/JardimZen3000/Assets/Unity-IndentShader-master/Assets/IndentSurface/Scripts/IndentActor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Wacki.IndentSurface
+{
+    /// <summary>
+    /// Simple control script for our sphere that leaves a track in the snow.
+    /// </summary>
+    public class IndentActor : MonoBehaviour
+    {
+        [Range(0.0f, 0.2f)]
+        public float drawDelta = 0.01f;
+        private Vector3 _prevDrawPos;
+        
+        void Update()
+        {
+            
+            if (Vector3.Distance(_prevDrawPos, transform.parent.position) < drawDelta)
+                return;
+
+            _prevDrawPos = transform.parent.position;
+
+            //Debug.DrawLine(transform.parent.position, transform.parent.position + Vector3.down);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                var texDraw = hit.collider.gameObject.GetComponent<IndentDraw>();
+                if (texDraw == null)
+                    return;
+
+                texDraw.IndentAt(hit);
+            }
+        }
+    }
+
+}
---
+++ Zen/JardimZen3000/Assets/movearrow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movearrow : MonoBehaviour {
+    public float rotationSpeed = 8;  //This will determine max rotation speed, you can adjust in the inspector
+
+    Camera cam;  //Drag the camera object here
+
+    public void StartGame()
+    {
+        Time.timeScale = 1F;
+    }
+
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+        Time.timeScale = 0F;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().AddForce(Input.GetAxis("Horizontal")*Vector3.right+Input.GetAxis("Vertical")*Vector3.forward);
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+
+        //Adjust mouse z position
+        mousePos.z = cam.transform.position.y - transform.position.y;
+
+        //Get a world position for the mouse
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(mousePos);
+
+        //Get the angle to rotate and rotate
+        float angle = -Mathf.Atan2(transform.position.z - mouseWorldPos.z, transform.position.x - mouseWorldPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, angle, 0), rotationSpeed * Time.deltaTime);
+    
+}
+}
